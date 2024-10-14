7e2cb1fd1fa9796a13038a1d6f1b8639239d2c7c
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-10-14 16:27:49 +0300

    modify host

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import { render } from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
-import * as config from '../src/config.js'
-export const settings= config.settings;
+
 
 import { logout as apiLogout } from './api/api.js';
 
